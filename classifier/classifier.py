88a398a3e65e59d4b342722a9e02b7776c94aea2
Lioncat2002 <sankhayan2002@gmail.com>
2023-06-02 18:30:08 +0530

    feat: classifier done

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,5 +1,66 @@
 import pandas as pd
+from bs4 import BeautifulSoup
+import json
+from pprint import pprint
+from .secrets import OPENAPI_KEY
+import openai 
 
-def read_all():
+openai.api_key = OPENAPI_KEY
+def remove_html(text):
+    soup = BeautifulSoup(text, 'html.parser')
+    txt=soup.get_text()
+    txt=txt.replace('\n', '')
+    txt=txt.replace('"', '')
+    return txt
+
+def get_all():
     df = pd.read_csv('justice.csv')
-    return df[["name","term"]]
+    df.columns = [x.capitalize() for x in df.columns]
+    for idx in range (0,len(df['Facts'])):
+        df['Facts'][idx] = remove_html(df['Facts'][idx])
+    return json.loads(df.to_json(orient='records'))
+
+def summarize(ids: list):
+    data=[]
+    for id in ids:
+        df = pd.read_csv('justice.csv')
+        df.columns = [x.capitalize() for x in df.columns]
+        for idx in range (0,len(df['Facts'])):
+            df['Facts'][idx] = remove_html(df['Facts'][idx])
+        data.append(df[df['Id']==id]['Facts'].values[0])
+    txt=f'''You are a lawyer, Summarize the following list of texts below into a single gist. : {'  '.join(data)} 
+    in about 300 words divided into points'''
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "assistant", "content": txt},
+        ]
+    )
+    data=""
+    for choice in response.choices:
+        data+=choice.message.content
+    return data
+
+def classify_query(query:str):
+    df = pd.read_csv('justice.csv')
+    df.columns = [x.capitalize() for x in df.columns]
+    issue_area=df['Issue_area'].unique().tolist()[1:]
+    txt=f'''You are a lawyer, classify the following query: "{query}" into one of the following issue areas: {'  '.join(issue_area)}'''
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "assistant", "content": txt},
+        ]
+    )
+    data=""
+    for choice in response.choices:
+        data+=choice.message.content
+    print(data)
+    return json.loads(df[df['Issue_area']==data].to_json(orient='records'))
+    #for idx in range (0,len(df['Facts'])):
+    #    df['Facts'][idx] = remove_html(df['Facts'][idx])
+    
+
+if __name__=="__main__":
+   pprint(classify_query("Murders after 2020")) 
+
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+from pydantic import BaseModel
 from classifier import classifier
 from fastapi import FastAPI
-
+import dotenv
+dotenv.load_dotenv(".env")
 app = FastAPI()
 
-
+class ID(BaseModel):
+    ids:list
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+class Query(BaseModel):
+    query:str
 
 @app.get("/cases/")
 def read_item():
-    return classifier.read_all()
+    return {"data":classifier.get_all()}
+
+@app.post("/cases/compile")
+def compile_item(data: ID):
+    ids=[]
+    for id in data.ids:
+        ids.append(int(id))
+    print(ids)
+    return {"data":classifier.summarize(ids)}
+
+@app.post("/cases/query")
+def query_item(query: Query):
+    return {"data":classifier.classify_query(query.query)}
